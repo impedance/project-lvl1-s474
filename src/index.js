422f589786a4bdb3811d314b2be5019daf95f232
user <2275399@gmail.com>
2019-04-09 13:49:45 +0300

    updated hellouser function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!\n');
 
 const helloUser = () => {
+  console.log('Welcome to the Brain Games!\n');
+
   const userName = readlineSync.question('May I have your name? ');
 
   console.log(`Hello ${userName}!`);
