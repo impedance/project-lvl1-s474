817089f895ead8c8fd12325619185890b10c8293
impedance <2275399@gmail.com>
2019-04-11 17:44:38 +0300

    adding calc game to index

--- src/index.js
+++ src/index.js
@@ -8,12 +8,17 @@ const helloUser = () => {
   return name;
 };
 
-const evenGame = () => {
-  console.log('Answer "yes" if number even otherwise answer "no".\n');
-  const userName = helloUser();
-  const generateNum = (min, max) => Math.floor(Math.random(min) * Math.floor(max));
+const generateNum = (min, max) => Math.floor(Math.random(min) * Math.floor(max));
+const rightAnswerLimit = 3;
+const userName = helloUser();
+const operations = [
+  [(a, b) => a + b, '+'], 
+  [(a, b) => a * b, '*'], 
+  [(a, b) => a - b, '-']
+];
 
-  const rightAnswerLimit = 3;
+const evenGame = () => {
+  console.log('Answer "yes" if number even otherwise answer "no".\n');  
 
   for (let rightnessCounter = 0; rightnessCounter <= rightAnswerLimit;) {
     const questionNum = generateNum(1, 20);
@@ -21,7 +26,6 @@ const evenGame = () => {
     const userReply = readlineSync.question('Your answer: ');
     const isEven = questionNum % 2 === 0;
     const rightAnswer = isEven ? 'yes' : 'no';
-
     if (userReply === 'yes' || userReply === 'no') {
       if (rightAnswer === userReply) {
         rightnessCounter += 1;
@@ -40,4 +44,29 @@ const evenGame = () => {
   return 0;
 };
 
-export default evenGame;
+const calcGame = () => {
+  console.log('What is the result of the expression?\n');
+  for (let rightnessCounter = 0; rightnessCounter <= rightAnswerLimit;) {
+    const [firstNum, secondNum] = [generateNum(1,20), generateNum(1, 20)];
+    const operation = operations[generateNum(0,3)];
+    const rightAnswer = operation[0](firstNum, secondNum);
+    console.log(`Question: ${firstNum} ${operation[1]} ${secondNum}`);
+    const userReply = readlineSync.question('Your answer: ');
+    if (typeof userReply === 'number') {
+      if(userReply === rightAnswer) {
+        rightnessCounter += 1;
+        console.log('Correct!');
+      } else {
+        console.log(`'${userReply}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
+        return console.log(`Let's try again, ${userName}!`);
+      }
+    } else {
+      return console.log(`"${userReply}" is wrong answer, please try again and type "yes" or "no" as answers`);
+    }
+    if (rightnessCounter === rightAnswerLimit) {
+      return console.log(`Congratulations ${userName}!`);
+    }
+  }
+};
+
+export { evenGame, calcGame };
