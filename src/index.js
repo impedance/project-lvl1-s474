3bf78e80017d28160aac3c44dbd00204a330bd00
user <2275399@gmail.com>
2019-04-11 19:30:42 +0300

    brain-calc creation

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ const generateNum = (min, max) => Math.floor(Math.random(min) * Math.floor(max))
 const rightAnswerLimit = 3;
 const userName = helloUser();
 const operations = [
-  [(a, b) => a + b, '+'], 
-  [(a, b) => a * b, '*'], 
-  [(a, b) => a - b, '-']
+  [(a, b) => a + b, '+'],
+  [(a, b) => a * b, '*'],
+  [(a, b) => a - b, '-'],
 ];
 
 const evenGame = () => {
-  console.log('Answer "yes" if number even otherwise answer "no".\n');  
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
 
   for (let rightnessCounter = 0; rightnessCounter <= rightAnswerLimit;) {
     const questionNum = generateNum(1, 20);
@@ -47,13 +47,13 @@ const evenGame = () => {
 const calcGame = () => {
   console.log('What is the result of the expression?\n');
   for (let rightnessCounter = 0; rightnessCounter <= rightAnswerLimit;) {
-    const [firstNum, secondNum] = [generateNum(1,20), generateNum(1, 20)];
-    const operation = operations[generateNum(0,3)];
+    const [firstNum, secondNum] = [generateNum(1, 20), generateNum(1, 20)];
+    const operation = operations[generateNum(0, 3)];
     const rightAnswer = operation[0](firstNum, secondNum);
     console.log(`Question: ${firstNum} ${operation[1]} ${secondNum}`);
     const userReply = readlineSync.question('Your answer: ');
-    if (typeof userReply === 'number') {
-      if(userReply === rightAnswer) {
+    if (!Number.isNaN(userReply)) {
+      if (Number(userReply) === rightAnswer) {
         rightnessCounter += 1;
         console.log('Correct!');
       } else {
@@ -61,12 +61,13 @@ const calcGame = () => {
         return console.log(`Let's try again, ${userName}!`);
       }
     } else {
-      return console.log(`"${userReply}" is wrong answer, please try again and type "yes" or "no" as answers`);
+      return console.log(`"${userReply}" is wrong answer, please try again and type 'number' as answers`);
     }
     if (rightnessCounter === rightAnswerLimit) {
       return console.log(`Congratulations ${userName}!`);
     }
   }
+  return 0;
 };
 
 export { evenGame, calcGame };
