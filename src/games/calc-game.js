a89e47bef88b1f4a13e002597a92fa7f97e46acd
impedance <2275399@gmail.com>
2019-04-12 15:54:03 +0300

    creating general game engine

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,36 +1,19 @@
-import readlineSync from 'readline-sync';
-import {
-  rightAnswerLimit, userName, generateNum, wrongInputReply, wrongAnswerReply,
-} from '../index';
+import { generateNum } from '../index';
 
-const calcGame = () => {
-  console.log('What is the result of the expression?\n');
-  const operations = [
-    [(a, b) => a + b, '+'],
-    [(a, b) => a * b, '*'],
-    [(a, b) => a - b, '-'],
-  ];
-  for (let rightnessCounter = 0; rightnessCounter <= rightAnswerLimit;) {
-    const [firstNum, secondNum] = [generateNum(1, 20), generateNum(1, 20)];
-    const operation = operations[generateNum(0, 3)];
-    const rightAnswer = operation[0](firstNum, secondNum);
-    console.log(`Question: ${firstNum} ${operation[1]} ${secondNum}`);
-    const userReply = readlineSync.question('Your answer: ');
-    if (!Number.isNaN(userReply)) {
-      if (rightAnswer === Number(userReply)) {
-        rightnessCounter += 1;
-        console.log('Correct!');
-      } else {
-        return wrongAnswerReply(userReply, rightAnswer, userName);
-      }
-    } else {
-      wrongInputReply(userReply, 'number');
-    }
-    if (rightnessCounter === rightAnswerLimit) {
-      return console.log(`Congratulations ${userName}!`);
-    }
-  }
-  return 0;
-};
+console.log('What is the result of the expression?\n');
+const operations = [
+  [(a, b) => a + b, '+'],
+  [(a, b) => a * b, '*'],
+  [(a, b) => a - b, '-'],
+];
 
-export default calcGame;
+const getCalcGameData = () => {
+  const firstNum = generateNum(1, 20);
+  const secondNum = generateNum(1, 20);
+  const operation = operations[generateNum(0, 3)];
+  const rightAnswer = operation[0](firstNum, secondNum);
+  const question = `Question: ${firstNum} ${operation[1]} ${secondNum}`;
+  return [question, rightAnswer]
+}
+
+export default getCalcGameData;
