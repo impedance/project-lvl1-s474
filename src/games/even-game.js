00a92e46ced591608123ef11117007e230f66314
impedance <2275399@gmail.com>
2019-04-12 11:21:21 +0300

    working under game engine

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,24 +1,23 @@
 import readlineSync from 'readline-sync';
-import { rightAnswerLimit, userName, generateNum } from '../index';
+import { rightAnswerLimit, userName, generateNum, wrongInputReply, wrongAnswerReply } from '../index';
 
 const evenGame = () => {
   console.log('Answer "yes" if number even otherwise answer "no".\n');
   for (let rightnessCounter = 0; rightnessCounter <= rightAnswerLimit;) {
     const questionNum = generateNum(1, 20);
-    console.log(`Question: ${questionNum}`);
-    const userReply = readlineSync.question('Your answer: ');
     const isEven = questionNum % 2 === 0;
     const rightAnswer = isEven ? 'yes' : 'no';
+    console.log(`Question: ${questionNum}`);
+    const userReply = readlineSync.question('Your answer: ');
     if (userReply === 'yes' || userReply === 'no') {
       if (rightAnswer === userReply) {
         rightnessCounter += 1;
         console.log('Correct!');
       } else {
-        console.log(`'${userReply}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
-        return console.log(`Let's try again, ${userName}!`);
+        wrongAnswerReply(userReply, rightAnswer, userName);
       }
     } else {
-      return console.log(`"${userReply}" is wrong answer, please try again and type "yes" or "no" as answers`);
+      wrongInputReply(userReply, 'yes or no');
     }
     if (rightnessCounter === rightAnswerLimit) {
       return console.log(`Congratulations ${userName}!`);
