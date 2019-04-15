28c1866a6510d21e008a923220d1854734f748c3
impedance <2275399@gmail.com>
2019-04-15 12:42:24 +0300

    eliminate conditin function

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,4 @@
 
 const generateNum = (min, max) => Math.floor(Math.random(min) * Math.floor(max));
 
-const getGameCondition = (type) => {
-  if (type === 'text') {
-    const inputCondition = reply => reply === 'yes' || reply === 'no';
-    const answerCondition = (answer, playerReply) => answer === playerReply;
-    return [inputCondition, answerCondition];
-  }
-  if (type === 'number') {
-    const inputCondition = reply => !Number.isNaN(reply);
-    const answerCondition = (answer, playerReply) => answer === Number(playerReply);
-    return [inputCondition, answerCondition];
-  }
-  return 0;
-};
-
-export { generateNum, getGameCondition };
+export default generateNum;
