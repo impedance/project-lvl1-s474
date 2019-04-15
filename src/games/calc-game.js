f3ac1cc6398dd3d37c3ea67fa612d9bcbf162f60
impedance <2275399@gmail.com>
2019-04-15 18:22:18 +0300

    changing function name

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -7,15 +7,15 @@ const operations = [
   [(a, b) => a - b, '-'],
 ];
 
-const calcGame = () => {
+const getCalcGameData = () => {
   const gameReply = 'What is the result of the expression?';
   const getAnswerQuestion = () => {
     const firstNum = generateNum(0, 100);
     const secondNum = generateNum(0, 100);
     const operationsItem = operations[generateNum(0, operations.length)];
     const [operation, sign] = operationsItem;
-    const rightAnswer = functionOperation(firstNum, secondNum);
-    const question = `${firstNum} ${operationSign} ${secondNum}`;
+    const rightAnswer = operation(firstNum, secondNum);
+    const question = `${firstNum} ${sign} ${secondNum}`;
     return [rightAnswer.toString(), question];
   };
 
