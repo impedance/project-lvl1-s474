006dc7dbcabf878db920724a2d6b1ba640d50e84
impedance <2275399@gmail.com>
2019-04-15 13:45:44 +0300

    changing checking answer  mechanism

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const answerLimit = 3;
 
 const gameEngine = (gameDescription, answerQuestionGenerator) => {
   console.log('Welcome to the Brain Games!');
-  const { gameReply, answerVariant, inputType } = gameDescription;
+  const { gameReply, answerVariant } = gameDescription;
   console.log(`${gameReply}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!\n`);
@@ -12,16 +12,11 @@ const gameEngine = (gameDescription, answerQuestionGenerator) => {
     const [rightAnswer, question] = answerQuestionGenerator();
     console.log(`Question: ${question}`);
     const userReply = readlineSync.question('Your answer: ');
-    if (input === 'text' ? userReply === 'yes' || userReply === 'no' : !Number.isNaN(userReply)) {
-      if (input === 'text' ? rightAnswer === userReply : rightAnswer === Number(userReply)) {
-        console.log('Correct!');
-      } else {
-        console.log(`'${userReply}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
-        console.log(`Let's try again, ${userName}!`);
-        break;
-      }
+    if (rightAnswer === userReply) {
+      console.log('Correct!');
     } else {
-      console.log(`"${userReply}" is wrong answer, please try again and type ${answerVariant} as answer`);
+      console.log(`'${userReply}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
+      console.log(`Let's try again, ${userName}!`);
       break;
     }
     if (i === answerLimit) {
