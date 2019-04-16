5fcc5599dd12a473c2c166f3d433a64221389168
impedance <2275399@gmail.com>
2019-04-16 16:06:42 +0300

    refactor algorithm

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = Math.floor(num / 2); i > 1; i -= 1) {
+  for (let i = 2; i < Math.floor(num / 2); i += 1) {
     if (num % i === 0) {
       return false;
     }
